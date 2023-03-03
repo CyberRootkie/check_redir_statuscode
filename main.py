256e5795cc3ffb264c5ba24ad286381ebdc927d2
CyberRootkie <126771070+CyberRootkie@users.noreply.github.com>
2023-03-03 21:16:29 +0100

    Gestion d'erreurs supplémentaires

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ output_file.write("start_url;final_url;nb_redir;final_status_code;regex_find\n")
 with open(args.input, 'r') as f:
     for line in f:
         start_url = line.strip()
+        final_url = ''
+        nb_redir = ''
+        regex_find = ''
         try:
             r = requests.get(start_url, headers=headers, timeout=5)
             final_url = r.url
@@ -35,9 +38,12 @@ with open(args.input, 'r') as f:
             print(start_url, final_url, nb_redir, final_status_code, regex_find)
         except requests.exceptions.ReadTimeout:
             print(start_url, "TIMEOUT")
-            final_url = ''
             final_status_code = 'TIMEOUT'
-            nb_redir = ''
-            regex_find = ''
+        except requests.exceptions.SSLError:
+            print(start_url, "SSL ERROR")
+            final_status_code = 'SSL ERROR'
+        except requests.exceptions.ConnectionError:
+            print(start_url, "Connection ERROR")
+            final_status_code = 'Connection ERROR'           
         output_file.write(start_url + ';' + final_url + ';' + str(nb_redir) + ';' + str(final_status_code) + ';' + str(regex_find) + '\n')
 output_file.close()
