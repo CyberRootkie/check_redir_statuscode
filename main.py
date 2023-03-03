75ee66302ea4de7d1c9ed26f693aa68c6ec0c416
CyberRootkie <126771070+CyberRootkie@users.noreply.github.com>
2023-03-03 20:56:02 +0100

    Gestion du timeout

--- main.py
+++ main.py
@@ -21,15 +21,23 @@ output_file.write("start_url;final_url;nb_redir;final_status_code;regex_find\n")
 with open(args.input, 'r') as f:
     for line in f:
         start_url = line.strip()
-        r = requests.get(start_url, headers=headers)
-        final_url = r.url
-        final_status_code = r.status_code
-        nb_redir = len(r.history)
-        regex_find = 'NA'
-        if args.regex != '':
-            if re.search(args.regex, r.text):
-                regex_find = True
-            else:
-                regex_find = False
-        print(start_url, final_url, nb_redir, final_status_code, regex_find)
+        try:
+            r = requests.get(start_url, headers=headers, timeout=5)
+            final_url = r.url
+            final_status_code = r.status_code
+            nb_redir = len(r.history)
+            regex_find = 'NA'
+            if args.regex != '':
+                if re.search(args.regex, r.text):
+                    regex_find = True
+                else:
+                    regex_find = False
+            print(start_url, final_url, nb_redir, final_status_code, regex_find)
+        except requests.exceptions.ReadTimeout:
+            print(start_url, "TIMEOUT")
+            final_url = ''
+            final_status_code = 'TIMEOUT'
+            nb_redir = ''
+            regex_find = ''
         output_file.write(start_url + ';' + final_url + ';' + str(nb_redir) + ';' + str(final_status_code) + ';' + str(regex_find) + '\n')
+output_file.close()
