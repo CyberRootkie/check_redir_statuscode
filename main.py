bcc6af3978020196e06de46266d2f4b09ae02d2f
CyberRootkie <126771070+CyberRootkie@users.noreply.github.com>
2023-03-03 20:44:43 +0100

    Première version

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+import requests
+import re
+
+# Pour le check de dispo Amazon : --regex "<div id=\"outOfStock\""
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, help="Fichier d'URLs à checker", default='')
+parser.add_argument("--output", type=str, help="Fichier de sortie pour les résultats", default="results.txt")
+parser.add_argument("--regex", type=str, help="(facultatif) Regex à checker dans le code HTML", default="")
+
+args = parser.parse_args()
+
+if args.input == '':
+    parser.print_usage()
+    exit()
+
+
+headers = {'user-agent': 'MyBot'}
+output_file = open(args.output, 'w')
+output_file.write("start_url;final_url;nb_redir;final_status_code;regex_find\n")
+with open(args.input, 'r') as f:
+    for line in f:
+        start_url = line.strip()
+        r = requests.get(start_url, headers=headers)
+        final_url = r.url
+        final_status_code = r.status_code
+        nb_redir = len(r.history)
+        regex_find = 'NA'
+        if args.regex != '':
+            if re.search(args.regex, r.text):
+                regex_find = True
+            else:
+                regex_find = False
+        print(start_url, final_url, nb_redir, final_status_code, regex_find)
+        output_file.write(start_url + ';' + final_url + ';' + str(nb_redir) + ';' + str(final_status_code) + ';' + str(regex_find) + '\n')
